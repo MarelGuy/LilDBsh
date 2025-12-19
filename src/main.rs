adea35e64f3798da66273859a98c766f3dd95e88
MarelGuy <irertnuc.loris@gmail.com>
2025-12-19 09:02:23 +0100

    added anyhow, improved error handling, improved raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-use core::time::Duration;
 use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
-use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use lildb::{
     lil_db_shell_client::LilDbShellClient, ConnectRequest, DisconnectRequest, DisconnectResponse,
 };
 use lildb::{CommandRequest, CommandResponse};
+use std::time::Duration;
 use std::{
     env::args,
-    error::Error,
     io::{stdout, Write},
     process,
 };
 use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio_stream::wrappers::ReceiverStream;
-use tonic::{transport::Channel, Response, Streaming};
+use tonic::{transport::Channel, Streaming};
 use tracing::{error, info};
 pub mod lildb {
     tonic::include_proto!("lildb");
 }
 
-fn clear_input() -> Result<(), Box<dyn Error>> {
+fn clear_input() -> anyhow::Result<()> {
     print!("\x1B[2K\x1B[1G");
     print!(">> ");
 
@@ -28,18 +27,18 @@ fn clear_input() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn read_input(input: &mut String, command_history: &[String]) -> Result<bool, Box<dyn Error>> {
+fn read_input(input: &mut String, command_history: &[String]) -> anyhow::Result<bool> {
     clear_input()?;
 
     let mut ch_len: usize = command_history.len();
 
     loop {
-        if let Event::Key(KeyEvent {
+        if let Ok(Event::Key(KeyEvent {
             code,
             kind,
             modifiers,
             state: _,
-        }) = read()?
+        })) = read()
         {
             if kind == KeyEventKind::Press {
                 match (code, modifiers) {
@@ -92,6 +91,7 @@ fn read_input(input: &mut String, command_history: &[String]) -> Result<bool, Bo
                 }
             }
         }
+
         stdout().flush()?;
     }
 
@@ -134,10 +134,12 @@ fn check_args() -> String {
 async fn connect_to_db(
     address: &String,
     public_ip: &String,
-) -> Result<LilDbShellClient<Channel>, Box<dyn Error>> {
+) -> anyhow::Result<LilDbShellClient<Channel>> {
     let mut input: String = String::new();
 
     if address == "null" {
+        enable_raw_mode()?;
+
         print!("Please insert your LilDB address:\n\r");
 
         stdout().flush()?;
@@ -145,6 +147,8 @@ async fn connect_to_db(
         read_input(&mut input, &Vec::new())?;
 
         print!("\n\r");
+
+        disable_raw_mode()?;
     } else {
         input.clone_from(address);
     }
@@ -163,8 +167,7 @@ async fn connect_to_db(
         );
 
         let channel_result: Result<Channel, tonic::transport::Error> =
-            Channel::from_shared(input.clone())
-                .unwrap()
+            Channel::from_shared(input.clone())?
                 .keep_alive_while_idle(true)
                 .keep_alive_timeout(Duration::from_secs(30))
                 .connect()
@@ -215,7 +218,7 @@ async fn handle_shell(
     mut client: LilDbShellClient<Channel>,
     mut command_history: Vec<String>,
     public_ip: String,
-) -> Result<(), Box<dyn Error>> {
+) -> anyhow::Result<()> {
     loop {
         let (tx, rx): (Sender<CommandRequest>, Receiver<CommandRequest>) = mpsc::channel(4);
         let (tx_command, mut rx_command): (Sender<String>, Receiver<String>) = mpsc::channel(4);
@@ -226,7 +229,7 @@ async fn handle_shell(
         tokio::spawn(async move {
             let mut command: String = String::new();
 
-            let mut exit: bool = read_input(&mut command, &command_history_clone).unwrap();
+            let mut exit: bool = read_input(&mut command, &command_history_clone)?;
 
             if command == "exit" {
                 exit = true;
@@ -235,39 +238,45 @@ async fn handle_shell(
             tx.send(CommandRequest {
                 command: command.clone(),
             })
-            .await
-            .unwrap();
+            .await?;
 
-            tx_command.send(command).await.unwrap();
+            tx_command.send(command).await?;
 
-            tx_disconnect.send(exit).await.unwrap();
+            tx_disconnect.send(exit).await?;
+
+            Ok::<(), anyhow::Error>(())
         });
 
-        if rx_disconnect.recv().await.unwrap() {
-            let disconnection: DisconnectResponse = client
-                .disconnect_from_db(DisconnectRequest {
-                    ip: public_ip.to_string(),
-                })
-                .await?
-                .into_inner();
+        if let Some(should_exit) = rx_disconnect.recv().await {
+            if should_exit {
+                let disconnection: DisconnectResponse = client
+                    .disconnect_from_db(DisconnectRequest {
+                        ip: public_ip.to_string(),
+                    })
+                    .await?
+                    .into_inner();
 
-            if disconnection.success {
-                info!("\n\r{}", disconnection.message);
+                if disconnection.success {
+                    info!("\n\r{}", disconnection.message);
 
-                break;
+                    break;
+                }
             }
         }
 
-        let command: String = rx_command.recv().await.unwrap();
-        command_history.push(command);
+        if let Some(command) = rx_command.recv().await {
+            command_history.push(command);
+        }
 
-        let response: Response<Streaming<CommandResponse>> =
-            client.run_command(ReceiverStream::new(rx)).await?;
+        match client.run_command(ReceiverStream::new(rx)).await {
+            Ok(response) => {
+                let mut inbound: Streaming<CommandResponse> = response.into_inner();
 
-        let mut inbound: Streaming<CommandResponse> = response.into_inner();
-
-        while let Some(res) = inbound.message().await? {
-            print!("\n\r{}", res.output);
+                while let Some(res) = inbound.message().await? {
+                    print!("\n\r{}", res.output);
+                }
+            }
+            Err(e) => error!("Command failed: {}", e),
         }
     }
 
@@ -275,9 +284,8 @@ async fn handle_shell(
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    enable_raw_mode()?;
 
     let address: String = check_args();
     let public_ip: String = reqwest::get("https://api.ipify.org").await?.text().await?;
@@ -286,7 +294,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let command_history: Vec<String> = Vec::new();
 
+    enable_raw_mode()?;
+
     handle_shell(client, command_history, public_ip).await?;
+
+    disable_raw_mode()?;
 
     Ok(())
 }
